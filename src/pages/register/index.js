d31d22de18dbf6322d12b3efd6ad05f1c825258f
Hanna Adenholm <hanna.adenholm@se.experis.com>
2025-09-03 15:51:19 +0200

    Added basic errorhandling, needs further work and styling

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -8,11 +8,27 @@ import './register.css';
 const Register = () => {
   const { onRegister } = useAuth();
   const [formData, setFormData] = useState({ email: '', password: '' });
+  const [passwordError, setPasswordError] = useState('');
+  const [emailError, setEmailError] = useState('');
 
   const onChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    if (name === 'password' && value.length < 8) {
+      setPasswordError(
+        'The password needs to be at leats eight characters, including at least one capital letter, one number and one special character'
+      );
+    } else if (name === 'password') {
+      setPasswordError('');
+    }
   };
+
+  function onSubmit() {
+    onRegister(formData.email, formData.password).catch((e) => {
+      console.error('Email already exists');
+      setEmailError('Email already exists');
+    });
+  }
 
   return (
     <div className="bg-blue register credentialpage">
@@ -32,6 +48,7 @@ const Register = () => {
               name="email"
               label={'Email *'}
             />
+            {emailError && <p readOnly={true}>{emailError}</p>}
             <TextInput
               value={formData.password}
               onChange={onChange}
@@ -39,12 +56,9 @@ const Register = () => {
               label={'Password *'}
               type={'password'}
             />
+            {passwordError && <p readOnly={true}>{passwordError}</p>}
           </form>
-          <Button
-            text="Sign up"
-            onClick={() => onRegister(formData.email, formData.password)}
-            classes="green width-full"
-          />
+          <Button text="Sign up" onClick={onSubmit} classes="green width-full" />
         </div>
       </CredentialsCard>
     </div>
