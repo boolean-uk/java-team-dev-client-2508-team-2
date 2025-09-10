eb36b74c8acc98d4d40c52b763668bba9a3555ec
Hanna Adenholm <hanna.adenholm@se.experis.com>
2025-09-10 11:40:07 +0200

    small fix

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -47,15 +47,13 @@ const Register = () => {
   function onSubmit(e) {
     e.preventDefault();
 
-    if (!passwordError) {
+    if (formData.password === '') {
+      setPasswordError("Password can't be empty");
+      setTimeout(() => setPasswordError(''), 2000);
+    } else if (!passwordError) {
       onRegister(formData.email.toLowerCase(), formData.password).catch((err) => {
         console.error(err.message);
         setEmailError(err.message);
-
-        if (formData.password === '') {
-          setPasswordError("Password can't be empty");
-          setTimeout(() => setPasswordError(''), 2000);
-        }
 
         setTimeout(() => setEmailError(''), 2000);
       });
